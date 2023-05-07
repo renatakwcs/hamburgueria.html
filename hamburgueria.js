c14edcf9866e881db24082ca2d390efc7c256668
renatakwcs <106083239+renatakwcs@users.noreply.github.com>
2023-05-07 06:48:08 -0300

    Create hamburgueria.js

---
+++ hamburgueria.js
@@ -0,0 +1,6 @@
+function submitOrder() {
+    // Pega os valores dos inputs do formulário
+    var name = document.getElementById("name").value;
+    var address = document.getElementById("address").value;
+    var phone = document.getElementById("phone").value;
+    var burger = document.getElementById
